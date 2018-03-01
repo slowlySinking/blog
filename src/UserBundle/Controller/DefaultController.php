be7cb0f3cc503a0229d6fbbf66ebaa39ccbe3b01
gun <gun19slinger@gmail.com>
2018-03-01 23:00:35 +0300

    fixed bugs

--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -3,15 +3,7 @@
 namespace UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
-    public function indexAction()
-    {
-        return $this->render('UserBundle:Default:index.html.twig');
-    }
 }
